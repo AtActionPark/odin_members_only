51cb66407f535f122c92d0f4eb9c457e27e9ac6d
Clement Matheron <clement.matheron@gmail.com>
2015-06-05 18:34:30 +0200

    Finish app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: "foobar", email: "foo@bar.com", password: "foobar", password_confirmation: "foobar")
